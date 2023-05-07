15bdf418a5d21092dda994e4f5162bd89fbd3a06
UIMSolutions <ui.manufaktur@gmail.com>
2023-05-07 15:00:56 +0200

    Updates

--- source/apps/myname/package.d
+++ source/apps/myname/package.d
@@ -1,1 +1,12 @@
 module apps.myname;
+
+public { // required uim libraries
+  import uim.core;
+  import uim.oop;
+  import uim.apps;
+}
+
+public { // admin-myname libraries
+  import apps.myname.controllers;
+  import apps.myname.views;
+}
